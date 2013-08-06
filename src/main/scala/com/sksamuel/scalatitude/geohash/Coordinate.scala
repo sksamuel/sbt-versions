464f4b69cd35fa15e9c7b5a93a758ffa026226cd
sksamuel <sam@sksamuel.com>
2013-08-06 08:00:45 +0100

    Added basic classes

--- src/main/scala/com/sksamuel/scalatitude/geohash/Coordinate.scala
+++ src/main/scala/com/sksamuel/scalatitude/geohash/Coordinate.scala
@@ -1,4 +1,6 @@
 package com.sksamuel.scalatitude.geohash
 
 /** @author Stephen Samuel */
-case class Geohash(hash: String)
+class Coordinate {
+
+}
---
+++ src/main/scala/com/sksamuel/scalatitude/geohash/Geohasher.scala
@@ -0,0 +1,39 @@
+package com.sksamuel.scalatitude.geohash
+
+/** @author Stephen Samuel */
+class Geohasher {
+  val base32 = Array('0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'j',
+    'k',
+    'm',
+    'n',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z')
+
+  def geohash(lat: Double, lon: Double): Geohash = null
+}
