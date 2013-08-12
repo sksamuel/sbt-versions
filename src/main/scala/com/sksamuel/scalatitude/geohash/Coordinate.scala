7810eb38a11d6cbfd5fa837d1fba1aae023be172
sksamuel <sam@sksamuel.com>
2013-08-12 23:07:51 +0100

    base classes

--- src/main/scala/com/sksamuel/scalatitude/geohash/Coordinate.scala
+++ src/main/scala/com/sksamuel/scalatitude/geohash/Coordinate.scala
@@ -3,5 +3,19 @@ package com.sksamuel.scalatitude.geohash
 /** @author Stephen Samuel */
 trait Coordinate
 
-case class LatLonCoordinate
-case class OSREFCoordinate
+class LatLonCoordinate extends Coordinate
+class OSRefCoordinate extends Coordinate
+
+trait CoordinateSystem
+class UtmCoordinateSystem extends CoordinateSystem
+class OSRefCoordinateSystem extends CoordinateSystem
+
+trait Ellipsoid
+class WGS84Ellipsoid extends Ellipsoid
+
+case class Longitude(value: Double) {
+  implicit def string2Longitude(str: String) = new Longitude(str.toDouble)
+}
+case class Latitude(value: Double) {
+  implicit def string2Latitude(str: String) = new Latitude(str.toDouble)
+}
